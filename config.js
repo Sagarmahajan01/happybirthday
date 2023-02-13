bf3853c3f4023bb7b85469caf50a05b14cd38696
Sagarmahajan01 <mahajansagar324@gmail.com>
2023-02-13 22:04:11 +0530

    changes

--- config.js
+++ config.js
@@ -1,34 +1,25 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
-    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    
     texts: [
-        "about a year ago ",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "I-saw-your-photo",  // 同上...
-        "I didn't believe in first side love,",
-        "but that day I believed",
-        "So",
-        "to get believy like this",
-        "happy",
-        "birthday ",
-        "Dear ~~ Devyani ~~ ",
-        "have a great day",
+        " it my prayer ",      
+        "that all your wishes", 
+        "come true on your special day",
+        "so ",
+        "happy ",
+        "birthday",
+        "~~ vaishali ~~",
+        "have a  ",
+        "great day ",
+    
         "..... ~~",
     ],
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/divya.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
+   
     imgs: {
-        "I-saw-your-photo":"divya.jpg",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "~~ vaishali ~~":"assets/v1.jpg",
+      
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+   
     desc: {
         turn_on: "Get Started",
         play: "music",
